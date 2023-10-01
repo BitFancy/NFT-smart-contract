a9c4c93a88e3fcfb531b091459cf9b4623b79dc6
snsakib <hello@snsakib.com>
2023-10-01 15:02:33 +0530

    import openzeppelin contracts

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,2 +1,5 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
