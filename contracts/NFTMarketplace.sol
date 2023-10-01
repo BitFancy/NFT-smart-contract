798bfe3d67a27bd55c797ac465735d1185d9ca5e
snsakib <hello@snsakib.com>
2023-10-01 15:31:59 +0530

    implement contract inheritance

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -3,3 +3,7 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract NFTMarketplace is ERC721URIStorage {
+  
+}
