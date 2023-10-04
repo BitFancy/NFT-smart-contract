f00ae7ee34f17bec4db147f888b59a10b5176692
snsakib <hello@snsakib.com>
2023-10-04 19:36:30 +0530

    define global variables

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,5 +5,12 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
-    constructor() ERC721("EducativeNFT", "EDUNFT") {}
+    using Counters for Counters.Counter;
+    Counters.Counter private _nftIds;
+    address payable contractOwner;
+    uint256 public listingPrice = 10000000 wei;
+    
+    constructor() ERC721("EducativeNFT", "EDUNFT") {
+        contractOwner = payable(msg.sender);
+    }
 }
